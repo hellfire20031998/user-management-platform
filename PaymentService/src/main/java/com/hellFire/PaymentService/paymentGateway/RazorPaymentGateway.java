a974e2885212ddc0e0e6404c495f1b3c8a8ed16f
Himanshu <tonu8707@gmail.com>
2025-12-18 01:37:05 +0530

    implemented stripe gateway

--- PaymentService/src/main/java/com/hellFire/PaymentService/paymentGateway/RazorPaymentGateway.java
+++ PaymentService/src/main/java/com/hellFire/PaymentService/paymentGateway/RazorPaymentGateway.java
@@ -1,11 +1,8 @@
 package com.hellFire.PaymentService.paymentGateway;
 
-
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
 public class RazorPaymentGateway implements IPaymentGateway {
     @Override
     public String generatePaymentLink() {
