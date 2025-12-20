b31ee0d1e2fb0202400fe9f21bd4a40a9e9efa70
Himanshu <tonu8707@gmail.com>
2025-12-20 22:26:54 +0530

    added eureka service

--- UserProfileService/src/main/java/com/hellFire/UserProfileService/UserProfileServiceApplication.java
+++ UserProfileService/src/main/java/com/hellFire/UserProfileService/UserProfileServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+
 public class UserProfileServiceApplication {
 
 	public static void main(String[] args) {
